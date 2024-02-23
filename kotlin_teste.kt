b38287d5cff23820aca499ae561b702aa07a5901
Diogo-10 <diogogf@hotmail.com>
2024-02-23 09:25:11 -0300

    elvis, null, for

---
+++ kotlin_teste.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val str: String? =null
+    // Verificação padrão
+    if (str == null){
+        println("NULO TEXTO INFORMATIVO")	
+    } else {
+        println(str)
+    }
+    println(str ?: "NULO TEXTO INFORMATIVO")
+}
